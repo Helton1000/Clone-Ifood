6cf0add6dc3e813f5841a53e26634de0e02d234c
Helton Barbosa <helton1000xd8@gmail.com>
2024-05-12 21:12:35 -0300

    feat: ensure user can redo order

--- app/my-orders/_components/ordem-item.tsx
+++ app/my-orders/_components/ordem-item.tsx
@@ -4,9 +4,13 @@ import { Avatar, AvatarImage } from "@/app/_components/ui/avatar";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Separator } from "@/app/_components/ui/separator";
+import { CartContext } from "@/app/_context/cart";
+import { formatCurrency } from "@/app/_helpers/price";
 import { OrderStatus, Prisma } from "@prisma/client";
 import { ChevronRightIcon } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
 
 interface OrderItemProps {
   order: Prisma.OrderGetPayload<{
@@ -37,6 +41,20 @@ const getOrderStatusLabel = (status: OrderStatus) => {
 };
 
 const OrdemItem = ({ order }: OrderItemProps) => {
+  const { addProductToCart } = useContext(CartContext);
+  const router = useRouter();
+  const handleRedoOrderClick = () => {
+    for (const orderProduct of order.products) {
+      addProductToCart({
+        product: {
+          ...orderProduct.product,
+          restaurant: order.restaurant,
+        },
+        quantity: orderProduct.quantity,
+      });
+    }
+    router.push(`/restaurants/${order.restaurantId}`);
+  };
   return (
     <div>
       <Card>
@@ -93,6 +111,21 @@ const OrdemItem = ({ order }: OrderItemProps) => {
 
           <div className="py-3">
             <Separator />
+          </div>
+
+          <div className="flex items-center justify-between">
+            <p className="text-sm">
+              {formatCurrency(Number(order.totalPrice))}
+            </p>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="text-xs text-primary"
+              disabled={order.status !== "COMPLETED"}
+              onClick={handleRedoOrderClick}
+            >
+              Refazer pedido
+            </Button>
           </div>
         </CardContent>
       </Card>
